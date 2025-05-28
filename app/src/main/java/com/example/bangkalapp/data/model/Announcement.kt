63e64fd21b0fa26bb9af7de3f7d280be3a1f502b
pandutria <panduadyatama029@gmail.com>
2025-05-28 22:50:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bangkalapp/data/model/Announcement.kt
@@ -0,0 +1,9 @@
+package com.example.bangkalapp.data.model
+
+data class Announcement(
+    val id: Int? = null,
+    val title: String? = null,
+    val text: String? = null,
+    val image_url: String? = null,
+    val date: String? = null,
+)
--- app/src/main/java/com/example/bangkalapp/view/ui/fragment/NewsFragment.kt
+++ app/src/main/java/com/example/bangkalapp/view/ui/fragment/NewsFragment.kt
@@ -5,56 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.bangkalapp.R
+import com.example.bangkalapp.data.local.Helper
+import com.example.bangkalapp.data.model.Announcement
+import com.example.bangkalapp.data.network.HttpHandler
+import com.example.bangkalapp.databinding.FragmentNewsBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NewsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NewsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentNewsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
+        binding = FragmentNewsBinding.inflate(layoutInflater,container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    fun showDataAnnouncement() {
+        try {
+            lifecycleScope.launch(Dispatchers.IO) {
+                val response = HttpHandler().request("announcement")
+                val list = mutableListOf<Announcement>()
+
+                if (response.code in 200..300) {
+                    val jsonArray = JSONArray(response.body)
+
+                    for (i in 0 until jsonArray.length()) {
+                        val data = jsonArray.getJSONObject(i)
+
+                        list.add(Announcement(
+                            id = data.getInt("id"),
+                            title = data.getString("title"),
+                            text = data.getString("text"),
+                            image_url = data.getString("image_url"),
+                            date = data.getString("date"),
+                        ))
+                    }
                 }
             }
+
+        } catch (e: Exception) {
+            Helper.log(e.message!!)
+        }
     }
+
 }
